4d96e727424a39e13086f89b3f2efe84e8e4a7d2
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-04 00:33:00 -0300

    att

--- sys/src/context/AuthProvider/util.ts
+++ sys/src/context/AuthProvider/util.ts
@@ -1,5 +1,22 @@
 import Password from "antd/lib/input/Password";
 import { api } from "../../servcies/api";
+import { IUser } from "./types";
+
+export function setUserLocalStorage(user: IUser | null){
+    localStorage.setItem('u', JSON.stringify(user));
+}
+
+export function getLocalStorage  () {
+    const json = localStorage.getItem('u')
+
+    if(!json){
+        return null;
+    }
+
+    const user = JSON.parse(json)
+
+    return user ?? null;
+} 
 
 //essa funçao acessa api
 export async function LoginRequest (email: string,  password: string){
