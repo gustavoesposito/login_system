215be1bb903c28185b8d98c622cc0762d4b28f7f
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-04 11:42:10 -0300

    att

--- sys/src/context/AuthProvider/index.tsx
+++ sys/src/context/AuthProvider/index.tsx
@@ -1,12 +1,21 @@
 import React , {createContext, useState} from "react";
 import { IAuthProvider, IContext, IUser } from "./types";
-import { LoginRequest, setUserLocalStorage } from "./util";
+import { getLocalStorage , LoginRequest, setUserLocalStorage } from "./util";
+import { useEffect } from "react";
 
 export const AuthContext = createContext<IContext>({} as IContext  )
 
 //ele vai ficar por volta e vai contorlar o estado , de quem fez login e de quem fez logout
 export const AuthProvider = ( {children}: IAuthProvider) => {
 const [user, setUser ] = useState<IUser | null>()
+
+useEffect(() => {
+ const user = getLocalStorage();
+
+ if (user){
+    setUser(user)
+ }
+}, [])
 
 async function authenticate(email: string, password: string ) {
     const response = await LoginRequest(email, password)
@@ -21,6 +30,7 @@ async function authenticate(email: string, password: string ) {
 
 function logout (){
 setUser(null)
+setUserLocalStorage(null)
 }
 
 return (
