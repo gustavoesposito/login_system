1db96b478ce82ac19868f2455ae94f8e2a5e6d68
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-04 13:19:59 -0300

    att

--- sys/src/App.tsx
+++ sys/src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import { AuthProvider } from './context/AuthProvider'
-import
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProtectedLayout } from './components/ProtectedLayout';
+
   
 function App() {
  
   return (
      <AuthProvider>
-
+       <BrowserRouter>
+      <Routes>
+       <Route path='/' element={<ProtectedLayout/>}>
+        
+       </Route>
+      </Routes>
+    </BrowserRouter>
      </AuthProvider> 
   )
 }
---
+++ sys/src/components/ProtectedLayout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Outlet } from "react-router-dom"
+import { useAuth } from "../../context/AuthProvider/useAuth"
+
+export const ProtectedLayout = (   ) => {
+
+    const auth = useAuth()
+
+    if(!auth.email){
+        return (
+            <h1>Você não tem acesso</h1>
+        )
+    }
+
+    return <Outlet/>
+
+}
