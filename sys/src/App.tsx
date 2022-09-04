6bfca8c838bdede7edb686612dce0cda4ab6ed36
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-04 14:01:23 -0300

    att

--- sys/src/App.tsx
+++ sys/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { AuthContext, AuthProvider } from './context/AuthProvider'
 import { BrowserRouter, Routes, Route,    } from "react-router-dom";
 import { ProtectedLayout } from './components/ProtectedLayout';
+import { Login } from './components/Login';
 
   
 function App() {
@@ -13,6 +14,9 @@ function App() {
        <Route path='/' element={<ProtectedLayout/>}>
         <Route path='/profile' element={<ProtectedLayout/>}>
           <h1>Olá esse é o componente profile</h1>
+        </Route>
+        <Route path='/login' element={<Login/>}>
+          
         </Route>
        </Route>
       </Routes>
