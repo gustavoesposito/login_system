6602e8c85e7a2165391054daa24f46ed3efdd0dc
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-03 23:58:56 -0300

    att

--- sys/src/context/AuthProvider/index.tsx
+++ sys/src/context/AuthProvider/index.tsx
@@ -1,1 +1,9 @@
 import React , {createContext} from "react";
+import { IAuthProvider, IContext } from "./types";
+
+export const Auth = createContext<IContext>({} as IContext  )
+
+//ele vai ficar por volta e vai contorlar o estado , de quem fez login e de quem fez logout
+export const AuthProvider = ( {children}: IAuthProvider) => {
+
+}
